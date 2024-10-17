6f98058dc1279dc580555d250195e19f7c6a44d6
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-18 02:40:48 +0300

    4

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,5 +1,7 @@
 -- trigger
+delimiter //
 create trigger sub before insert on orders
 for each row begin 
 update items set items.quantity -= NEW.number where items.name = new.item_name;
-end;
+end//
+delimiter ;
