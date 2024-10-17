e8e385635f141b0c96f29199bd809d3aa3277daa
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-18 00:32:04 +0300

    1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- creates a table `users` in a database
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL auto_increment PRIMARY KEY,
+    email varchar(255) NOT NULL UNIQUE,
+    name varchar(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL
+);
