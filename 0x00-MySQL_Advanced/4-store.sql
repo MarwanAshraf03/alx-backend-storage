42d9d9ce22dbc5e39dba6f2e4ee895c5afc2c9fe
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-18 02:44:45 +0300

    4

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -4,7 +4,7 @@ delimiter |
 CREATE TRIGGER sub BEFORE INSERT ON orders
   FOR EACH ROW
   BEGIN
-    update items set items.quantity = items.quantity - NEW.number where items.name = new.item_name;
+    UPDATE items SET items.quantity = items.quantity - NEW.number WHERE items.name = new.item_name;
   END;
 |
 
