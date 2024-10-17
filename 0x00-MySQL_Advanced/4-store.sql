aa1f6609df9ef3f5fa4018f3471b82ebe3f3fd7b
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-18 02:32:49 +0300

    4

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,3 +1,4 @@
 -- trigger
 create trigger before insert on order
-for each row set holberton.items.quantity = holberton.items.quantity - new.number;
+for each row begin 
+update items set items.quantity = items.quantity - new.number where items.name = new.item_name;
