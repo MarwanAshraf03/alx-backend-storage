bd403cc062e714e20cad85317e321c9b17fba508
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-23 02:59:51 +0300

    8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,3 +1,8 @@
-// lists all databases
-db.school.delete({ name : "Holberton school" })
-exit
+#! usr/bin/env python
+"""Module"""
+import pymongo
+
+
+def list_all(mongo_collection):
+    """returns all documents in a collection"""
+    return mongo_collection.find()
