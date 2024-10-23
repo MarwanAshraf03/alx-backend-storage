cf6cb9984f73bcb770cb31d7281a2669036280e0
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-23 03:39:43 +0300

    10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,4 +6,4 @@ def update_topics(mongo_collection, name, topics):
     """returns all documents in a collection"""
     qfind = {"name": name}
     updatef = {"$set": {"topics": topics}}
-    mongo_collection.update_one(qfind, updatef)
+    mongo_collection.find_one_and_update(qfind, updatef)
