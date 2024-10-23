639d32b32d6ef184961fb0ec79a105993af5910d
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-23 04:41:34 +0300

    12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,18 +6,13 @@ from pymongo import MongoClient
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs
-    nginx_collection = db.nginx
-    print(f"{nginx_collection.count_documents({})} logs")
+    nc = db.nginx
+    print(f"{nc.count_documents({})} logs")
     print("Methods:")
-    print(f'\tmethod GET: {len(list(nginx_collection.find(\
-        {"method": "GET"})))}')
-    print(f'\tmethod POST: {len(list(nginx_collection.find(\
-        {"method": "POST"})))}')
-    print(f'\tmethod PUT: {len(list(nginx_collection.find(\
-        {"method": "PUT"})))}')
-    print(f'\tmethod PATCH: {len(list(nginx_collection.find(\
-        {"method": "PATCH"})))}')
-    print(f'\tmethod DELETE: {len(list(nginx_collection.find(\
-        {"method": "DELETE"})))}')
-    print(f'{len(list(nginx_collection.find(\
-        {"method": "GET", "path": "/status"})))} status check')
+    print(f'\tmethod GET: {len(list(nc.find({"method": "GET"})))}')
+    print(f'\tmethod POST: {len(list(nc.find({"method": "POST"})))}')
+    print(f'\tmethod PUT: {len(list(nc.find({"method": "PUT"})))}')
+    print(f'\tmethod PATCH: {len(list(nc.find({"method": "PATCH"})))}')
+    print(f'\tmethod DELETE: {len(list(nc.find({"method": "DELETE"})))}')
+    cur = nc.find({"method": "GET", "path": "/status"})
+    print(f'{len(list(cur))} status check')
