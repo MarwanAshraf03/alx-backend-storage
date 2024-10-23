cfdfb4ad0aa2ca8253104f6e68bf9bd35cf88139
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-23 04:06:35 +0300

    12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" 11-main """
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    nginx_collection = db.nginx
+    print(len(db.find()))
