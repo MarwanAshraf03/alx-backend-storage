1916eecd72180f94dfe3461835bcd08d5612e6cd
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-23 04:38:19 +0300

    12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python3
-""" 11-main """
+""" 12 """
 from pymongo import MongoClient
-from pymongo import cursor
 
-def lenn(cur):
-    count = 0
-    for i in cur:
-        # count += 1
-        print(i)
-    return count
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
@@ -16,11 +9,15 @@ if __name__ == "__main__":
     nginx_collection = db.nginx
     print(f"{nginx_collection.count_documents({})} logs")
     print("Methods:")
-    print(f'\tmethod GET: {len(list(nginx_collection.find({"method": "GET"})))}')
-    print(f'\tmethod POST: {len(list(nginx_collection.find({"method": "POST"})))}')
-    print(f'\tmethod PUT: {len(list(nginx_collection.find({"method": "PUT"})))}')
-    print(f'\tmethod PATCH: {len(list(nginx_collection.find({"method": "PATCH"})))}')
-    print(f'\tmethod DELETE: {len(list(nginx_collection.find({"method": "DELETE"})))}')
-    print(f'{len(list(nginx_collection.find({"method": "GET", "path": "/status"})))} status check')
-    
-# { "_id" : ObjectId("5a8fa9e0d4321e1852684d11"), "path" : "/status", "ip" : "172.31.2.14", "method" : "GET", "date" : "22/Feb/2018:03:51:02" }
+    print(f'\tmethod GET: {len(list(nginx_collection.find(
+        {"method": "GET"})))}')
+    print(f'\tmethod POST: {len(list(nginx_collection.find(
+        {"method": "POST"})))}')
+    print(f'\tmethod PUT: {len(list(nginx_collection.find(
+        {"method": "PUT"})))}')
+    print(f'\tmethod PATCH: {len(list(nginx_collection.find(
+        {"method": "PATCH"})))}')
+    print(f'\tmethod DELETE: {len(list(nginx_collection.find(
+        {"method": "DELETE"})))}')
+    print(f'{len(list(nginx_collection.find(
+        {"method": "GET", "path": "/status"})))} status check')
