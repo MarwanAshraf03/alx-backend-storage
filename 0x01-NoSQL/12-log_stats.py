6a83814763bf32219386468efca15981da424b04
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-23 04:35:23 +0300

    12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -16,12 +16,11 @@ if __name__ == "__main__":
     nginx_collection = db.nginx
     print(f"{nginx_collection.count_documents({})} logs")
     print("Methods:")
-    print(len(list(nginx_collection.find({"method": "GET"}))))
-    # print(f'\tmethod GET: {lenn(nginx_collection.find({"path": "GET"}))}')
-    print(f"\tmethod POST: {0}")
-    print(f"\tmethod PUT: {0}")
-    print(f"\tmethod PATCH: {0}")
-    print(f"\tmethod DELETE: {0}")
-    print(f"{0} status check")
+    print(f'\tmethod GET: {len(list(nginx_collection.find({"method": "GET"})))}')
+    print(f"\tmethod POST: {len(list(nginx_collection.find({"method": "POST"})))}")
+    print(f"\tmethod PUT: {len(list(nginx_collection.find({"method": "PUT"})))}")
+    print(f"\tmethod PATCH: {len(list(nginx_collection.find({"method": "PATCH"})))}")
+    print(f"\tmethod DELETE: {len(list(nginx_collection.find({"method": "DELETE"})))}")
+    print(f"{len(list(nginx_collection.find({"method": "GET", "path": "/status"})))} status check")
     
 # { "_id" : ObjectId("5a8fa9e0d4321e1852684d11"), "path" : "/status", "ip" : "172.31.2.14", "method" : "GET", "date" : "22/Feb/2018:03:51:02" }
