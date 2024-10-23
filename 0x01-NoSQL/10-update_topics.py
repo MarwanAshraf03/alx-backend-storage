b6eeafc35c7025e82a51ed57dc2ee88312769920
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-23 03:15:00 +0300

    10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """returns all documents in a collection"""
+    mongo_collection.update_one({"name":name}, {"$set": {"topics":topics}})
