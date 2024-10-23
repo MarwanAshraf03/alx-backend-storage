7f3be7d4d9d31c75489eadecd602ba545e04268c
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-23 04:13:22 +0300

    12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs
     nginx_collection = db.nginx
-    print(nginx_collection.count_documents())
+    print(nginx_collection.count_documents({}))
