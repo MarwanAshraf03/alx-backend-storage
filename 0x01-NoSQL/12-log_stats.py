8144c8ae135d900ae952320a299efdf8d108eb32
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-23 04:39:38 +0300

    12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -9,15 +9,15 @@ if __name__ == "__main__":
     nginx_collection = db.nginx
     print(f"{nginx_collection.count_documents({})} logs")
     print("Methods:")
-    print(f'\tmethod GET: {len(list(nginx_collection.find(
+    print(f'\tmethod GET: {len(list(nginx_collection.find(\
         {"method": "GET"})))}')
-    print(f'\tmethod POST: {len(list(nginx_collection.find(
+    print(f'\tmethod POST: {len(list(nginx_collection.find(\
         {"method": "POST"})))}')
-    print(f'\tmethod PUT: {len(list(nginx_collection.find(
+    print(f'\tmethod PUT: {len(list(nginx_collection.find(\
         {"method": "PUT"})))}')
-    print(f'\tmethod PATCH: {len(list(nginx_collection.find(
+    print(f'\tmethod PATCH: {len(list(nginx_collection.find(\
         {"method": "PATCH"})))}')
-    print(f'\tmethod DELETE: {len(list(nginx_collection.find(
+    print(f'\tmethod DELETE: {len(list(nginx_collection.find(\
         {"method": "DELETE"})))}')
-    print(f'{len(list(nginx_collection.find(
+    print(f'{len(list(nginx_collection.find(\
         {"method": "GET", "path": "/status"})))} status check')
