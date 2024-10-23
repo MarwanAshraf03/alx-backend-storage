406571757e2e4aab24d0bc10f1d9a4079fe76991
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-23 04:19:39 +0300

    12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,6 +2,11 @@
 """ 11-main """
 from pymongo import MongoClient
 
+def lenn(cur):
+    count = 0
+    for _ in cur:
+        count += 1
+    return count
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
@@ -9,7 +14,7 @@ if __name__ == "__main__":
     nginx_collection = db.nginx
     print(f"{nginx_collection.count_documents({})} logs")
     print("Methods:")
-    print(f'\tmethod GET: {len(nginx_collection.find({"path": "GET"}))}')
+    print(f'\tmethod GET: {lenn(nginx_collection.find({"path": "GET"}))}')
     print(f"\tmethod POST: {0}")
     print(f"\tmethod PUT: {0}")
     print(f"\tmethod PATCH: {0}")
