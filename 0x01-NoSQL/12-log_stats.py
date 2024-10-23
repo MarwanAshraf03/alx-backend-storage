5a312171b7993c9de5a2344e147fefd0de3e17f9
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-23 04:18:00 +0300

    12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,4 +7,13 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs
     nginx_collection = db.nginx
-    print(nginx_collection.count_documents({}))
+    print(f"{nginx_collection.count_documents({})} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {len(nginx_collection.find({"path": "GET"}))}")
+    print(f"\tmethod POST: {0}")
+    print(f"\tmethod PUT: {0}")
+    print(f"\tmethod PATCH: {0}")
+    print(f"\tmethod DELETE: {0}")
+    print(f"{0} status check")
+    
+# { "_id" : ObjectId("5a8fa9e0d4321e1852684d11"), "path" : "/status", "ip" : "172.31.2.14", "method" : "GET", "date" : "22/Feb/2018:03:51:02" }
