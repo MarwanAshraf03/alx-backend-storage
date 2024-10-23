f89505397062e221faf754a175f4e17bebd85439
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-23 03:52:14 +0300

    11

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """returns all documents in a collection"""
+    return mongo_collection.find({"topics": {"$in": topic}})
