106fc2176ca3a837762438ee96b551573ab4309d
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-23 04:20:19 +0300

    12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
     nginx_collection = db.nginx
     print(f"{nginx_collection.count_documents({})} logs")
     print("Methods:")
-    print(f'\tmethod GET: {lenn(nginx_collection.find({"path": "GET"}))}')
+    print(nginx_collection.find({"path": "GET"}))
+    # print(f'\tmethod GET: {lenn(nginx_collection.find({"path": "GET"}))}')
     print(f"\tmethod POST: {0}")
     print(f"\tmethod PUT: {0}")
     print(f"\tmethod PATCH: {0}")
