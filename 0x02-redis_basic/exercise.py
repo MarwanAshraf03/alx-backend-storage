24a259e982a1c0a7b1ddf3093cb2210116642f02
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-25 18:31:27 +0300

    0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
+"""A module for creating cache class using redis database"""
 import redis
 import uuid
+import typing
 
 
 class Cache:
-    def __init__(self):
-        self._redis = redis.Redis()
+    """Class cache using redis database"""
+    def __init__(self) -> None:
+        """Constructor"""
+        self._redis: redis.Redis = redis.Redis()
         self._redis.flushdb()
-    def store(self, data):
-        key = str(uuid.uuid4())
-        # print(type(key))
-        # print(key)
-        # return "he"
+
+    def store(self, data: typing.Union[str, bytes, float, int]) -> str:
+        """stores data using random uuid as a key then returns the key"""
+        key: str = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
