16ceb6efe978c07862004ac682f1f230fdfb5f0f
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-10-29 02:09:33 +0300

    3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,6 +13,15 @@ def count_calls(method: typing.Callable) -> typing.Callable:
         return method(self, data)
     return wrapper
 
+def call_history(method: typing.Callable) -> typing.Callable:
+    @wraps(method)
+    def wrapper(self, *args):
+        self._redis.rpush(f"{method.__qualname__}:inputs", str(args))
+        out = method(self, *args)
+        self._redis.rpush(f"{method.__qualname__}:outputs", out)
+        return out
+    return wrapper
+
 
 class Cache:
 
@@ -21,7 +30,9 @@ class Cache:
         """Constructor"""
         self._redis = redis.Redis()
         self._redis.flushdb()
+        self._redis.rpush()
 
+    @call_history
     @count_calls
     def store(self, data: typing.Union[str, bytes, int, float]) -> str:
         """stores data using random uuid as a key then returns the key"""
